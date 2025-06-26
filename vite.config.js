b49aff74a29f0b198badf5c60e2bb7a1ab705bfd
Ayush <aayushpandey07080603@gmail.com>
2025-06-26 12:13:43 +0530

    Add root route for /

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 5173, // 👈 Let Render pick the port
+    host: true,                     // 👈 Listen on all interfaces (0.0.0.0)
+  }
 });
